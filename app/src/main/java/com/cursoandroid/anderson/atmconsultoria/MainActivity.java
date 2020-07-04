f282021b13dbeb6be732fae443181e6486f4dd9d
Anderson Fernandes <andersonfernandes@visaojr.com.br>
2020-07-04 00:12:51 -0300

    Enviando e-mail

--- app/src/main/java/com/cursoandroid/anderson/atmconsultoria/MainActivity.java
+++ app/src/main/java/com/cursoandroid/anderson/atmconsultoria/MainActivity.java
@@ -1,5 +1,7 @@
 package com.cursoandroid.anderson.atmconsultoria;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -34,11 +36,11 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                enviarEmail();
             }
         });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -52,6 +54,29 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    public void enviarEmail() {
+        String celular = "tel:31971046276";
+        String imagem = "https://br.web.img3.acsta.net/pictures/18/08/15/22/09/0358925.jpg";
+        String endereco = "https://www.google.com/maps/dir/-19.453068,-43.1068623/parque+ibirapuera/@-21.5048532,-47.1258677,7z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x94ce59f1069d11d1:0xcb936109af9ce541!2m2!1d-46.6576336!2d-23.5874162";
+        //Intent intent = new Intent(  Intent.ACTION_DIAL,Uri.parse());
+        //Intent intent = new Intent(  Intent.ACTION_VIEW,Uri.parse(imagem));
+        //Intent intent = new Intent(  Intent.ACTION_VIEW,Uri.parse(endereco));
+
+        Intent intent = new Intent(  Intent.ACTION_SEND );
+
+        intent.putExtra( Intent.EXTRA_EMAIL, new String[]{"atmconsultoria.atendimento@atm.com.br"} );
+        intent.putExtra( Intent.EXTRA_SUBJECT, "Contato pelo App");
+        intent.putExtra( Intent.EXTRA_TEXT, "Mensagem automática");
+
+        //intent.setType( "message/rfc822" );
+        //intent.setType( "text/plain" );
+        //intent.setType( "image/*" );
+        intent.setType( "application/pdf" );
+
+
+        startActivity( Intent.createChooser(intent, "Escolha um App de e-mail") );
     }
 
     @Override
